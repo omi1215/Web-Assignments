a6df535bd3af4dbe9e4663329c919b8bb3aa1ef1
omparkash <parkashom.1215@gmail.com>
2024-03-17 01:15:14 +0500

    win and lose logic added in js

--- RPS.js
+++ RPS.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("img").click(function() { 
         let op1 = $(".op1");
         let op2 = $(".op2");   
-
+        let str = "";
         op1.animate({ rotate: '+=30deg' }, 500)
             .animate({ rotate: '-=60deg' }, 500)
             .animate({ rotate: '+=30deg' }, 500);
@@ -46,7 +46,36 @@ $(document).ready(function() {
         setTimeout(function() {
             op1.find('img').attr("src", name);
             op2.find('img').attr("src", pcImage);
+            if(name == "RPS-Assets/rock.png" && pcImage == "RPS-Assets/scissors.png")
+            {
+                 str = "You Win!"
+            }
+            else if(name == "RPS-Assets/paper.png" && pcImage == "RPS-Assets/rock.png")
+            {
+                 str = "You Win!"
+            }
+            else if(name == "RPS-Assets/scissors.png" && pcImage == "RPS-Assets/paper.png")
+            {
+                 str = "You Win!"
+            }
+            else if(name == "RPS-Assets/scissors.png" && pcImage == "RPS-Assets/scissors.png")
+            {
+                 str = "Draw!"
+            }
+            else if(name == "RPS-Assets/paper.png" && pcImage == "RPS-Assets/paper.png")
+            {
+                 str = "Draw!"
+            }
+            else if(name == "RPS-Assets/rock.png" && pcImage == "RPS-Assets/rock.png")
+            {
+                 str = "Draw!"
+            }
+            else{
+                 str = "You Lose!"
+            }
+            
+            var $paragraph = $("<p></p>").text(str);
+            $(".winner").empty().append($paragraph);
         }, 1000);
-
     });
 });
